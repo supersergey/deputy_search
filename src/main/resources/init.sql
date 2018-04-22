4855a1876cdf70075a96b3d85a7360ee8d390261
supersergey <sergey.tolokunsky@gmail.com>
2018-04-22 08:38:49 +0300

    db init screen

---
+++ src/main/resources/init.sql
@@ -0,0 +1,23 @@
+use deputy_search;
+CREATE TABLE info_card
+(
+  id VARCHAR(128) PRIMARY KEY NOT NULL,
+  first_name VARCHAR(64) NOT NULL,
+  last_name VARCHAR(64) NOT NULL,
+  patronymic VARCHAR(64),
+  url VARCHAR(1024),
+  column_6 INT(11),
+  created_date DATETIME NOT NULL,
+  parsed_date DATETIME NOT NULL
+);
+CREATE UNIQUE INDEX info_card_id_uindex ON info_card (id);
+CREATE TABLE company
+(
+  id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(1024) NOT NULL,
+  status VARCHAR(16),
+  url VARCHAR(1024),
+  info_card VARCHAR(128),
+  CONSTRAINT fk_info_card FOREIGN KEY (info_card) REFERENCES info_card (id)
+);
+CREATE INDEX fk_info_card ON company (info_card);
