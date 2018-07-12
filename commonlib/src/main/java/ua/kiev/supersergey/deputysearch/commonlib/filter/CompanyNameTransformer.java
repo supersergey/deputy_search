ae3c461f101e8edb32885e3e985953f0016f00e7
Sergiy Tolokunskyy <sergey.tolokunsky@gmail.com>
2018-07-12 16:15:52 +0300

    another refactoring

--- commonlib/src/main/java/ua/kiev/supersergey/deputysearch/commonlib/filter/CompanyNameTransformer.java
+++ commonlib/src/main/java/ua/kiev/supersergey/deputysearch/commonlib/filter/CompanyNameTransformer.java
@@ -4,12 +4,24 @@ package ua.kiev.supersergey.deputysearch.commonlib.filter;
  * Created by supersergey on 20.05.18.
  */
 public class CompanyNameTransformer {
+
+    private static final String[] prefixes = {
+            "ТОВ", "ТЗОВ", "ОАО", "ЗАО", "ООО", "ЧП", "КП", "ДП", "РТЦ", "СХК", "ПП", "АБ"};
+    private static final String[] suffixes = {
+            "Лтд", "Гмбх", "Лимитед", "Лімітед", "ltd", "gmbh"};
+
     public static String transform(String name) {
+        for (String prefix : prefixes) {
+            name = name.replaceAll("(?iu)" + prefix + "\\s+", "");
+        }
+        for (String suffix : suffixes) {
+            name = name.replaceAll("(?iu)" + suffix + "\\.?", "");
+        }
         name = name.replace("''", "\"");
         int firstQuoteIndex = name.indexOf("\"");
         int lastQuoteIndex = name.lastIndexOf("\"");
         if (firstQuoteIndex == -1 || firstQuoteIndex == lastQuoteIndex) {
-            return name;
+            return name.trim();
         }
         do {
             int nextQouteIndex = name.indexOf("\"", firstQuoteIndex + 1);
@@ -19,6 +31,6 @@ public class CompanyNameTransformer {
                 firstQuoteIndex = nextQouteIndex;
             }
         } while (true);
-        return name.substring(firstQuoteIndex + 1, lastQuoteIndex);
+        return name.substring(firstQuoteIndex + 1, lastQuoteIndex).trim();
     }
 }
--- commonlib/src/test/java/ua/kiev/supersergey/deputysearch/commonlib/filter/CompanyNameTransformerTest.java
+++ commonlib/src/test/java/ua/kiev/supersergey/deputysearch/commonlib/filter/CompanyNameTransformerTest.java
@@ -18,6 +18,17 @@ public class CompanyNameTransformerTest {
         assertEquals("abc", CompanyNameTransformer.transform("abc"));
         assertEquals("ТОН", CompanyNameTransformer.transform("ТОВ \"ТОН\""));
         assertEquals("Замок Радомисль", CompanyNameTransformer.transform("Історико-культурний комплекс \"Замок Радомисль\""));
+        assertEquals("Новембе Венчез", CompanyNameTransformer.transform("Новембе Венчез Лтд"));
+        assertEquals("Брокер Фаворит", CompanyNameTransformer.transform("ТОВ \"Брокер Фаворит\""));
+        assertEquals("ЛИСИЧАНСЬКИЙ СКЛЯНИЙ ЗАВОД", CompanyNameTransformer.transform("ТОВ \"ЛИСИЧАНСЬКИЙ СКЛЯНИЙ ЗАВОД\""));
+        assertEquals("Вінницька промислова група", CompanyNameTransformer.transform("СХК Вінницька промислова група"));
+        assertEquals("ПРОЛЕТАРІЙ", CompanyNameTransformer.transform("ТОВ \"ТОРГОВИЙ ДІМ \"ПРОЛЕТАРІЙ\""));
+        assertEquals("Кліринговий Дім", CompanyNameTransformer.transform("АБ \"Кліринговий Дім\""));
+        assertEquals("ЕВРОІНВЕСТ ПЛЮС", CompanyNameTransformer.transform("ТОВ \"КУА \"ЕВРОІНВЕСТ ПЛЮС\""));
+        assertEquals("ЛИДЛЕНД", CompanyNameTransformer.transform("ЛИДЛЕНД ЛТД"));
+        assertEquals("ЛИДЛЕНД", CompanyNameTransformer.transform("ЛИДЛЕНД лтд."));
+        assertEquals("ЛИДЛЕНД", CompanyNameTransformer.transform("ТзОВ ЛИДЛЕНД лтд"));
+        assertEquals("ЛИДЛЕНД", CompanyNameTransformer.transform("ТзОВ ЛИДЛЕНД ЛімІТЕд."));
     }
 
 }
